888a52e50a14b3a387520fd4fdb90074e289e7d3
Andrés <lopez.andresrigoberto@gmail.com>
2022-11-08 16:44:43 -0600

    change id

--- src/components/user/pages/User.js
+++ src/components/user/pages/User.js
@@ -4,7 +4,7 @@ import UserList from "../components/UserList";
 const Users = () => {
   const USERS = [
     {
-      id: 'uid1',
+      id: 'u1',
       name: 'Andres',
       image:
         'https://imgs.search.brave.com/pdhIT8GMxNeASsJ6uILi7_6cAxFuD6km_mbSK0wFx1E/rs:fit:1200:1200:1/g:ce/aHR0cHM6Ly9pLnBp/bmltZy5jb20vb3Jp/Z2luYWxzL2ExL2Fj/L2EzL2ExYWNhM2Iw/MTU2MTA3ZWQ3ZGNl/YzhjZTA3NWVjOWZl/LmpwZw',
