18d20abf6af7809e352cf823ae68bace110239b7
Andrés <lopez.andresrigoberto@gmail.com>
2022-11-08 16:44:23 -0600

    Add update place page

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import Users from "./components/user/pages/User";
 import NewPlace from "./components/places/pages/NewPlace";
 import UserPlaces from "./components/places/pages/UserPlaces";
 import MainNavigation from "./components/shared/components/Navigation/MainNavigation";
+import UpdatePlace from "./components/places/pages/UpdatePlace";
 
 function App() {
   return (
@@ -19,11 +20,14 @@ function App() {
           <Route path="/" exact>
             <Users />
           </Route>
+          <Route path="/:userId/places">
+            <UserPlaces />
+          </Route>
           <Route path="/places/new">
             <NewPlace />
           </Route>
-          <Route path="/:userId/places">
-            <UserPlaces />
+          <Route path="/places/:placeId">
+            <UpdatePlace />
           </Route>
           <Redirect to="/" />
         </Switch>
