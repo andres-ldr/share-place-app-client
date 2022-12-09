68e5e954d04acfe7d1b24cc3f37d3b7a7023539d
Andrés <lopez.andresrigoberto@gmail.com>
2022-12-08 18:40:43 -0600

    find a place by user id from db

--- src/components/places/pages/UserPlaces.js
+++ src/components/places/pages/UserPlaces.js
@@ -1,41 +1,39 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-
+import { useHttpClient } from "../../shared/hooks/http-hook";
 import PlaceList from "../components/PlaceList";
-
-const ITEMS = [
-  {
-    id: "p1",
-    title: "Empire State Building",
-    description: "One of the famous scrappers",
-    imageUrl:
-      "https://upload.wikimedia.org/wikipedia/commons/thumb/1/10/Empire_State_Building_%28aerial_view%29.jpg/800px-Empire_State_Building_%28aerial_view%29.jpg",
-    address: "20 W 34th St., New York, NY 10001, États-Unis",
-    location: {
-      lat: 40.7484405,
-      lng: -73.9878584,
-    },
-    creatorId: "u1",
-  },
-  {
-    id: "p2",
-    title: "La tour Eiffel",
-    description: "Trop belle!",
-    imageUrl:
-      "https://blog.toploc.com/wp-content/uploads/2020/04/Pensez-a-visiter-la-Tour-Eiffel-lors-de-votre-sejour-a-Paris.jpg",
-    address: "Champ de Mars, 5 Av. Anatole France, 75007 Paris, France",
-    location: {
-      lat: 40.7484405,
-      lng: -73.9878584,
-    },
-    creatorId: "u1",
-  },
-];
+import ErrorModal from "../../shared/components/UIElements/ErrorModal";
+import LoadingSpinner from "../../shared/components/UIElements/LoadingSpinner";
 
 const UserPlaces = () => {
   const userId = useParams().userId;
-  const loadedPlaces = ITEMS.filter((place) => place.creatorId === userId);
-  return <PlaceList items={loadedPlaces} />;
+  const { isLoading, error, sendRequest, clearError } = useHttpClient();
+  const [loadedPlaces, setLoadedPlaces] = useState();
+
+  useEffect(() => {
+    const fetchPlaces = async () => {
+      try {
+        const responseData = await sendRequest(
+          `http://localhost:5000/api/places/user/${userId}`
+        );
+        setLoadedPlaces(responseData.places);
+      } catch (err) {}
+    };
+
+    fetchPlaces();
+  }, [sendRequest, userId]);
+
+  return (
+    <React.Fragment>
+      <ErrorModal error={error} onClear={clearError} />
+      {isLoading && (
+        <div className="center">
+          <LoadingSpinner />
+        </div>
+      )}
+      {!isLoading && loadedPlaces && <PlaceList items={loadedPlaces} />}
+    </React.Fragment>
+  );
 };
 
 export default UserPlaces;
