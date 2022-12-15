459fbcab356403a7a10545d0c310878d7eba41e9
Andrés <lopez.andresrigoberto@gmail.com>
2022-12-15 13:24:56 -0600

    add auth hook

--- src/App.js
+++ src/App.js
@@ -13,61 +13,10 @@ import MainNavigation from "./components/shared/components/Navigation/MainNaviga
 import UpdatePlace from "./components/places/pages/UpdatePlace";
 import Auth from "./components/user/pages/Auth";
 import { AuthContext } from "./components/shared/context/auth-context";
-
-let logoutTimer;
+import { useAuth } from "./components/shared/hooks/auth-hook";
 
 function App() {
-  const [token, setToken] = useState(false);
-  const [userId, setUserId] = useState(false);
-  const [tokenExpirationDate, setTokenExpirationDate] = useState();
-
-  const login = useCallback((uid, token, expirationDate) => {
-    setToken(token);
-    setUserId(uid);
-    const tokenExpirationDate =
-      expirationDate || new Date(new Date().getTime() + 1000 * 60 * 60);
-    setTokenExpirationDate(tokenExpirationDate);
-    localStorage.setItem(
-      "userData",
-      JSON.stringify({
-        userId: uid,
-        token: token,
-        expiration: tokenExpirationDate.toISOString(),
-      })
-    );
-  }, []);
-
-  useEffect(() => {
-    const storedData = JSON.parse(localStorage.getItem("userData"));
-    if (
-      storedData &&
-      storedData.token &&
-      new Date(storedData.expiration) > new Date()
-    ) {
-      login(
-        storedData.userId,
-        storedData.token,
-        new Date(storedData.expiration)
-      );
-    }
-  }, [login]);
-
-  const logout = useCallback(() => {
-    setToken(null);
-    setTokenExpirationDate(null);
-    setUserId(null);
-    localStorage.removeItem("userData");
-  }, []);
-
-  useEffect(() => {
-    if (token && tokenExpirationDate) {
-      const remainingTime =
-        tokenExpirationDate.getTime() - new Date().getTime();
-      logoutTimer = setTimeout(logout, remainingTime);
-    } else {
-      clearTimeout();
-    }
-  }, [token, logout, tokenExpirationDate]);
+  const { token, login, logout, userId } = useAuth();
 
   let routes;
 
