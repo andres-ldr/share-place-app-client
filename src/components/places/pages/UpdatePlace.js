c54ba5318f2ec92c9f9d6aff46b3da366d4f5af7
Andrés <lopez.andresrigoberto@gmail.com>
2022-12-14 19:23:31 -0600

    use token to update item

--- src/components/places/pages/UpdatePlace.js
+++ src/components/places/pages/UpdatePlace.js
@@ -72,7 +72,10 @@ const UpdatePlace = () => {
           title: formState.inputs.title.value,
           description: formState.inputs.description.value,
         }),
-        { "Content-TYpe": "application/json" }
+        {
+          "Content-TYpe": "application/json",
+          Authorization: "Bearer " + auth.token,
+        }
       );
       history.push(`/${auth.userId}/places`);
     } catch (err) {}
